0a1faa11439d8b3f9fea165468f541a7056e1cc8
ultimatus1 <paul.lukic94@gmail.com>
2019-06-28 19:54:25 +0200

    todos reactjs

---
+++ src/components/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is Todo List app v1.0.0. It is part of the React crash course</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
